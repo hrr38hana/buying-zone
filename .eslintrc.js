f7a0e342151d41f1ffa473f70382ee250755deb1
Adam Gipril <adam.gipril@icloud.com>
2019-05-16 11:54:41 -0600

    Project setup completed ===
    Installed dependencies and devDependencies, updated package.json
    ---
    
    Dependencies:
    
    - express
    - mongoose
    - react
    
    Dev Dependencies:
    
    - babel
    - enzyme
    - jest
    - webpack
    
    First steps taken towards serving the app, ESLint set up
    ---
    
    - index.html begun
    - index.jsx filled in with dummy component to test webpack config (untested so far)
    - ESLint checks all files other than those in .eslintignore against Airbnb style guide
    
    Webpack set up to bundle into /client/dist folder
    ---
    
    - package.json script `build:dev` added
    - babel presets configured
    - also npm installed puppeteer
    
    Server set up to serve the app on port 3001
    ---
    
    Basic server tests written with jest, puppeteer, supertest
    ---
    
    - circleci config file begun, and will be tested after pushing the changes up to this branch at origin
    - tests indicate that server is serving html, and that the html contains the words "Buying Zone"
    
    Cleanup and prep for project setup pr to master
    ---

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,22 @@
-/**
- * These rules enforce the Hack Reactor Style Guide
- *
- * Visit this repo for more information:
- *   https://github.com/reactorcore/eslint-config-hackreactor
- */
-
 module.exports = {
-  extends: './node_modules/eslint-config-hackreactor/index.js'
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+    browser: true,
+  },
+  extends: 'airbnb',
+  globals: {
+    page: true,
+    browser: true,
+    context: true,
+    jestPuppeteer: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
 };
