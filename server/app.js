dbba19fe8941bffcf0f2153d07ce8225ab616723
Adam Gipril <adam.gipril@icloud.com>
2019-05-18 00:50:44 -0600

    First API endpoint routed ---
    - GET /products/:id sends back the product matching the id param in res.body

--- server/app.js
+++ server/app.js
@@ -6,4 +6,19 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+const { Product } = require('../database/db');
+
+app.get('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findOne({ id });
+  res.json(product);
+});
+
+// this endpoint may change so the color can be passed in req.body and more thoroughly identified
+app.get('/products/:id/:color', async (req, res) => {
+  const { id, color } = req.params;
+  const product = await Product.findOne({ id, color });
+  res.json(product);
+});
+
 module.exports = app;
