8ef31b2bfe7d40f941ab56e978a1506a5a1b93f5
Adam Gipril <44346169+bikeshaman@users.noreply.github.com>
2019-05-17 21:07:51 -0600

    Update server.test.js
    - trimmed unnecessary comments

--- test/server.test.js
+++ test/server.test.js
@@ -1,22 +1,6 @@
-// const request = require('supertest');
-
-// const app = require('../server/app');
-
 describe('server', () => {
-  // beforeAll(async () => {
-  //   await page.goto('http://localhost:3001/', { waitUntil: 'load' });
-  // });
-
-  // test('should display "Buying Zone" text on the page', async () => {
-  //   await expect(page).toMatch('Buying Zone');
-  // });
-
+  // useless test just to practice with jest for now
   test('should display "Buying Zone" text on the page', () => {
     expect(4).toBe(4);
   });
-
-  // test('root route serves an html file', async () => {
-  //   const response = await request(app).get('/');
-  //   expect(response.header['content-type']).toBe('text/html; charset=UTF-8');
-  // });
 });
