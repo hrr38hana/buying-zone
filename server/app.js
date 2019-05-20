8df73c63473189122c98fb6950a447c6ca84c32d
Adam Gipril <adam.gipril@icloud.com>
2019-05-20 09:45:49 -0600

    PUT request to purchase a product written ---
    - endpoint for purchasing a product is now /products/:id
    - specific details about the purchase (size, color, quantity to purchase) are passed in the request body

--- server/app.js
+++ server/app.js
@@ -8,17 +8,46 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 
 const { Product } = require('../database/db');
 
+/**
+ * Gets the product associated with the id passed in the url. Sends the retrieved product back as
+ * JSON.
+ * @param {number} id - The product's human-readable id (think model number)
+ * @returns {Product} - Sends the queried product back in the response
+ */
 app.get('/products/:id', async (req, res) => {
   const { id } = req.params;
-  const product = await Product.findOne({ id });
-  res.json(product);
+  try {
+    const product = await Product.findOne({ id });
+    res.json(product);
+  } catch (err) {
+    // TODO: improve error handling
+    res.sendStatus(400);
+  }
 });
 
-// this endpoint may change so the color can be passed in req.body and more thoroughly identified
-app.get('/products/:id/:color', async (req, res) => {
-  const { id, color } = req.params;
-  const product = await Product.findOne({ id, color });
-  res.json(product);
+/**
+ * Removes products from inventory upon purchase. The particular product options and quantity
+ * purchased are passed in the request's body, and the product's model number (id) is passed as a
+ * param in the endpoint's url.
+ * @param {string} colorId - The selected color's mongo-assigned id (req.body)
+ * @param {string} size - The selected size to purchase (req.body)
+ * @param {number} quantityPurchased - The quantity to purchase (req.body)
+ * @param {number} id - The product's human-readable id (think model number) (req.params in url)
+ */
+app.put('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const { colorId, size, quantityPurchased } = req.body;
+  try {
+    const product = await Product.findOne({ id });
+    const color = await product.colors.id(colorId);
+    const quantityInInventory = color.quantityInInventory.get(size);
+    color.quantityInInventory.set(size, quantityInInventory - quantityPurchased);
+    await product.save();
+    res.sendStatus(200);
+  } catch (err) {
+    // TODO: improve error handling
+    res.sendStatus(400);
+  }
 });
 
 module.exports = app;
