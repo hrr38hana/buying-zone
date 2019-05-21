257a54b4e6488767aa5137f672574fc853e179f7
Adam Gipril <adam.gipril@icloud.com>
2019-05-20 21:58:05 -0600

    Basic features of Buying Zone Implemented ---
    - no subcomponents completed yet
    - no styling completed yet

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const BuyingZone = () => <div className="buying-zone"> Welcome to the Buying Zone!! </div>;
+class BuyingZone extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentProduct: null,
+      id: 1,
+    };
+  }
+
+  componentDidMount() {
+    const { id } = this.state;
+    fetch(`/products/${id}`)
+      .then(response => response.json())
+      .then(currentProduct => this.setState({ currentProduct }));
+  }
+
+  render() {
+    const { currentProduct } = this.state;
+    return (
+      <div className="buying-zone">
+        <h1>
+          {currentProduct ? currentProduct.name : 'Loading...'}
+        </h1>
+        <span>
+          {currentProduct ? `$${currentProduct.price}.00` : 'Loading...'}
+        </span>
+        <span>
+          {currentProduct ? `Model ${currentProduct.id}` : 'Loading...'}
+        </span>
+        <p>
+          {currentProduct ? currentProduct.description : 'Loading...'}
+        </p>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<BuyingZone />, document.getElementById('buying-zone'));
