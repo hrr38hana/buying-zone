2b007d443c9f7694e0eb879da645de90009a18db
Adam Gipril <adam.gipril@icloud.com>
2019-05-23 14:34:33 -0600

    ReviewsAverage component styling updates ---
    - gradient fill is used to graphically represent the reviews average for a product
    - also updated size selector styling

--- client/src/components/ReviewsAverage.jsx
+++ client/src/components/ReviewsAverage.jsx
@@ -9,23 +9,35 @@ const ReviewsGrid = styled.div`
   cursor: default;
 `;
 const StarSquare = styled.div`
-  background-color: rgb(65, 185, 61);
-  background-color: rgb(242, 242, 242);
+  background:
+    linear-gradient(
+      to right,
+      rgb(65,185,61) ${props => `${props.fill * 100}%`},
+      rgb(242,242,242) ${props => `${props.fill * 100}%`}
+    );
   color: rgb(255, 255, 255);
   text-align: center;
   width: 1em;
   padding: 0.1em;
+  border-radius: 0.1em;
   line-height: 1em;
 `;
 
-const ReviewsAverage = () => (
-  <ReviewsGrid>
-    <StarSquare style={{ 'grid-column': '1 / span 1' }}> ⭑ </StarSquare>
-    <StarSquare style={{ 'grid-column': '2 / span 1' }}> ⭑ </StarSquare>
-    <StarSquare style={{ 'grid-column': '3 / span 1' }}> ⭑ </StarSquare>
-    <StarSquare style={{ 'grid-column': '4 / span 1' }}> ⭑ </StarSquare>
-    <StarSquare style={{ 'grid-column': '5 / span 1' }}> ⭑ </StarSquare>
-  </ReviewsGrid>
-);
+const ReviewsAverage = () => {
+  const squares = [1, 1, 1, 0.4, 0];
+  return (
+    <ReviewsGrid>
+      {squares.map((fill, i) => (
+        <StarSquare
+          style={{ gridColumn: `${i + 1} / span 1` }}
+          fill={fill}
+          key="squares"
+        >
+          ⭑
+        </StarSquare>
+      ))}
+    </ReviewsGrid>
+  );
+};
 
 export default ReviewsAverage;
