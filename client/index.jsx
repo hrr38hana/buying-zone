fd386e111da78c51bbeb38928c8206f58c8d20f3
Adam Gipril <adam.gipril@icloud.com>
2019-05-29 18:37:17 -0600

    Simplified file structure

--- client/index.jsx
+++ client/index.jsx
@@ -1,10 +1,8 @@
-import '@babel/polyfill';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createGlobalStyle } from 'styled-components';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import configureStore from './redux/store';
 import BuyingZone from './redux/containers/BuyingZoneContainer';
 
 const GlobalStyle = createGlobalStyle`
@@ -20,7 +18,7 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={configureStore()}>
     <GlobalStyle />
     <BuyingZone />
   </Provider>,
--- client/redux/store.js
+++ client/redux/store.js
@@ -1,8 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-
 import reducer from './reducers/root';
 
-const store = createStore(reducer, applyMiddleware(thunk));
-
-export default store;
+export default preloadedState => (
+  createStore(reducer, preloadedState, applyMiddleware(thunk))
+);
